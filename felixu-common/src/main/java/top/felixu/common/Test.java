18e9ed2926995fe0257ee52431f3636b43d5fcb6
felixu <xufei_0320@163.com>
2021-09-02 20:14:09 +0800

    feat: BeanUtils 增加对象复制，忽略 null 属性

--- felixu-common/src/main/java/top/felixu/common/Test.java
+++ felixu-common/src/main/java/top/felixu/common/Test.java
@@ -1,5 +1,8 @@
 package top.felixu.common;
 
+import lombok.Data;
+import top.felixu.common.bean.BeanUtils;
+
 /**
  * @author felixu
  * @date 2019.09.24
@@ -7,5 +10,18 @@ package top.felixu.common;
 public class Test {
 
     public static void main(String[] args) {
+        User source = new User();
+        source.setName("felixu");
+        User target = new User();
+        target.setAddress("火星");
+        System.out.println(BeanUtils.mapNotNull(User.class, target, User.class));
+        BeanUtils.copyNotNull(User.class, source, User.class, target);
+        System.out.println(target);
+    }
+
+    @Data
+    public static class User {
+        private String name = "wocao";
+        private String address;
     }
 }
--- felixu-common/src/main/java/top/felixu/common/bean/BeanUtils.java
+++ felixu-common/src/main/java/top/felixu/common/bean/BeanUtils.java
@@ -1,8 +1,15 @@
 package top.felixu.common.bean;
 
+import com.github.dozermapper.core.DozerBeanMapper;
 import com.github.dozermapper.core.DozerBeanMapperBuilder;
 import com.github.dozermapper.core.Mapper;
+import com.github.dozermapper.core.loader.api.BeanMappingBuilder;
+import com.github.dozermapper.core.loader.api.FieldsMappingOptions;
+import com.github.dozermapper.core.loader.api.TypeMappingOptions;
+import com.github.dozermapper.core.util.DozerClassLoader;
+import com.github.dozermapper.core.util.DozerConstants;
 
+import java.net.URL;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -20,9 +27,9 @@ public class BeanUtils {
     /**
      * 使用原对象，产生新对象，并进行属性拷贝
      *
-     * @param source 原对象
+     * @param source    原对象
      * @param destClazz 目标类型
-     * @param <T> 目标范型
+     * @param <T>       目标范型
      * @return 目标对象实例
      */
     public static <T> T map(Object source, Class<T> destClazz) {
@@ -30,23 +37,75 @@ public class BeanUtils {
     }
 
     /**
+     * 使用原对象，产生新对象，并进行属性拷贝(只拷贝不为 null 的属性)
+     *
+     * @param srcClass  原始类型
+     * @param source    原对象
+     * @param destClazz 目标类型
+     * @param <T>       目标范型
+     * @return 目标对象实例
+     */
+    public static <T> T mapNotNull(Class<?> srcClass, Object source, Class<T> destClazz) {
+        return getNotNullMapper(srcClass, destClazz).map(source, destClazz);
+    }
+
+    /**
      * 使用原对象集合，产生新对象集合，并进行属性拷贝
      *
-     * @param source 原对象集合
+     * @param source    原对象集合
      * @param destClazz 目标类型
-     * @param <T> 目标范型
+     * @param <T>       目标范型
      * @return 目标对象实例集合
      */
-    public static <T> List<T> map(List<Object> source, Class<T> destClazz) {
+    public static <T> List<T> map(List<?> source, Class<T> destClazz) {
         if (null == source)
             return Collections.emptyList();
         return source.stream().map(src -> map(src, destClazz)).collect(Collectors.toList());
     }
 
+    /**
+     * 使用原对象集合，产生新对象集合，并进行属性拷贝(只拷贝不为 null 的属性)
+     *
+     * @param srcClass  原始类型
+     * @param source    原对象集合
+     * @param destClazz 目标类型
+     * @param <T>       目标范型
+     * @return 目标对象实例集合
+     */
+    public static <T> List<T> mapNotNull(Class<?> srcClass, List<?> source, Class<T> destClazz) {
+        if (null == source)
+            return Collections.emptyList();
+        return source.stream().map(src -> mapNotNull(srcClass, src, destClazz)).collect(Collectors.toList());
+    }
+
+    /**
+     * 将指定对象的属性，设置到目标对象下
+     *
+     * @param source        原始对象
+     * @param destination   目标对象
+     */
     public static void copy(Object source, Object destination) {
         MAPPER.map(source, destination);
     }
-//    public static Map<String, Object> toMap(Object source) {
-//
-//    }
+
+    /**
+     * 将指定对象的属性，设置到目标对象下(只拷贝不为 null 的属性)
+     *
+     * @param srcClass      原始类型
+     * @param source        原始对象
+     * @param destClass     目标类型
+     * @param destination   目标对象
+     */
+    public static void copyNotNull(Class<?> srcClass, Object source, Class<?> destClass, Object destination) {
+        getNotNullMapper(srcClass, destClass).map(source, destination);
+    }
+
+    private static Mapper getNotNullMapper(Class<?> srcClass, Class<?> destClass) {
+        return DozerBeanMapperBuilder.create().withMappingBuilder(new BeanMappingBuilder() {
+            @Override
+            protected void configure() {
+                mapping(srcClass, destClass, TypeMappingOptions.oneWay(), TypeMappingOptions.mapNull(false), TypeMappingOptions.mapEmptyString(false));
+            }
+        }).build();
+    }
 }
